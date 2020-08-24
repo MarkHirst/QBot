8d7caec4bcf67f08da8f84054eb0398c42d361d8
MarkHirst <30925910+MarkHirst@users.noreply.github.com>
2020-08-24 12:05:29 +0100

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotresourcesix.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotresourceseven.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
