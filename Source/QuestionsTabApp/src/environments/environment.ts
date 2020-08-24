9fec1af81b779cd1f86058be7f79b728635733d2
MarkHirst <30925910+MarkHirst@users.noreply.github.com>
2020-08-24 10:27:12 +0100

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotresourcefive.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotresourcefive-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbotresourcesix.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbotresourcesix-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
